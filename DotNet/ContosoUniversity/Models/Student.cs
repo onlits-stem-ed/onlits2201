58c1fd453349e4d39fb528ebb8cbddeeada90d36
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-02-21 08:28:40 +0530

    20230221

--- DotNet/ContosoUniversity/Models/Student.cs
+++ DotNet/ContosoUniversity/Models/Student.cs
@@ -3,8 +3,8 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
-        public string? LastName { get; set; }
-        public string? FirstMidName { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
         public DateOnly EnrollmentDate { get; set; }
 
 
