cfdd38d252bdb9c3da7bafac395de077315b4eff
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-03-11 13:46:29 +0530

    20230311

---
+++ DotNet/console-app/database-connectivity/SelectData.cs
@@ -0,0 +1,15 @@
+using System.Data;
+using Microsoft.Data.Sqlite;
+
+namespace DatabaseConnectivity
+{
+    class SelectData
+    {
+        void GetData()
+        {
+            SqliteConnection connection = new SqliteConnection();
+            DataSet dataset = new DataSet();
+        }
+    }
+}
+
