94517653ea419d14392ca74bfccb944cfc407ca4
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-03-12 08:37:35 +0530

    20230312

--- DotNet/console-app/database-connectivity/SelectData.cs
+++ DotNet/console-app/database-connectivity/SelectData.cs
@@ -1,15 +1,23 @@
-using System.Data;
 using Microsoft.Data.Sqlite;
 
 namespace DatabaseConnectivity
 {
     class SelectData
     {
-        void GetData()
+        public void GetData()
         {
-            SqliteConnection connection = new SqliteConnection();
-            DataSet dataset = new DataSet();
+            SqliteConnection connection = new SqliteConnection("DataSource=crud_test.db");
+            connection.Open();
+            SqliteCommand command = new SqliteCommand();
+            command.Connection = connection;
+            command.CommandText = "SELECT * FROM student";
+            SqliteDataReader dataReader = command.ExecuteReader();
+            Console.WriteLine("Admission Number\tName");
+            while(dataReader.Read())
+            {
+                Console.WriteLine(dataReader["admission_no"] + "\t" + dataReader["name"]);
+            }
+            connection.Close();
         }
     }
 }
-
