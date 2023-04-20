c9d86d447b1811a732c0adce454bacfb9d104d77
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-04-20 14:29:12 +0530

    20230420

--- DotNet/webapi/TodoApi/wwwroot/js/site.js
+++ DotNet/webapi/TodoApi/wwwroot/js/site.js
@@ -3,6 +3,24 @@ let todos = [];
 
 function getItems() {
     fetch(uri)
-    .then(response => response.json());
+    .then(response => console.log(response.json()));
 }
 
+function addItem() {
+    const addNameTextbox = document.getElementById('add-name');
+    
+    const item = {
+        isComplete: false,
+        name: addNameTextbox.value.trim()
+    };
+
+    fetch(uri, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(item)
+    })
+        .then(() => console.log('Success!'));
+}
